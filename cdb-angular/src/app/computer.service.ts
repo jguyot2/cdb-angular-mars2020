3fa435999058c6ea0e2f23572e27c935acf3571f
Julien Guyot <jguyot@excilys.com>
2020-07-29 14:18:38 +0200

    FIX computerservice requests

--- cdb-angular/src/app/computer.service.ts
+++ cdb-angular/src/app/computer.service.ts
@@ -4,17 +4,17 @@ import { Computer } from './models/computer.model'
 import { Page } from './models/page.model'
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http'
 
-@Injectable({ 
+@Injectable({
     providedIn: 'root'
 })
 export class ComputerService {
     // URL du serveur à changer
     private baseUrl: string = 'http://localhost:8080/webapp/';
     private urlComputers: string = this.baseUrl + "computers/";
-    private token: string = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjoxNjAyMDExMjY5LCJpYXQiOjE1OTYwMTEyNjl9.2TYbWqHRgtiYTeJ7gUieGVpGx1HHhE3Na7SbAA1jJp-0yuMko0bPlXcoK42zGYqujdack405oOEzacey6fmzPA"
+    private token: string = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZXN0IiwiZXhwIjoxNjAyMDExMjY5LCJpYXQiOjE1OTYwMTEyNjl9.2TYbWqHRgtiYTeJ7gUieGVpGx1HHhE3Na7SbAA1jJp-0yuMko0bPlXcoK42zGYqujdack405oOEzacey6fmzPA";
 
     header: HttpHeaders = new HttpHeaders()
-        .append('Authorization', 'Bearer ' + this.token);
+        .append('Authorization', this.token);
 
     constructor(private http: HttpClient) { }
 
@@ -23,20 +23,23 @@ export class ComputerService {
     }
 
     getPaginatedComputerList(page: Page): Observable<Computer[]> {
-        return this.http.get<Computer[]>(this.urlComputers + "orderBy/a", {
+        console.log(page);
+        return this.http.get<Computer[]>(this.urlComputers + "page", {
             params: new HttpParams()
-                .set("currentPage", page.currentPage.toString()).set("pageSize", page.pageSize.toString()), headers: this.header
+                .append("pageSize", page.pageSize.toString())
+                .append("currentPage", page.currentPage.toString()),
+            headers : this.header
         });
     }
 
     getNumberComputers(): Observable<number> {
         return this.http.get<number>(this.urlComputers + "number", { headers: this.header });
     }
- 
+
     addComputer(computer: Computer): Observable<Computer> {
-        var header: HttpHeaders = new HttpHeaders()
+        const header: HttpHeaders = new HttpHeaders()
             .set('Content-Type', 'application/json')
             .set('Authorization', this.token);
-        return this.http.post<Computer>(this.urlComputers, (computer), { "headers": header });
+        return this.http.post<Computer>(this.urlComputers, (computer), { headers: header });
     }
 } 
